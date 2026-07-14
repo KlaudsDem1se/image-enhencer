b1081f0dde29edd214f3cb843fa98a554ba584f9
Zaurbek Bizikov <zatabizikov@edu.hse.ru>
2026-07-14 15:37:29 +0300

    image uploading

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-document
-.getElementById("btn")
-.addEventListener("click", () => {
+const button = document.getElementById("btn");
+const fileInput = document.getElementById("fileInput");
 
-    alert("Работает!");
+button.addEventListener("click", () => {
+    fileInput.click();
+});
+
+fileInput.addEventListener("change", (event) => {
+
+    const file = event.target.files[0];
+
+    if (!file) return;
+
+    console.log(file.name);
+
+});
+
+const preview = document.getElementById("preview");
+
+fileInput.addEventListener("change", (event) => {
+
+    const file = event.target.files[0];
+
+    if (!file) return;
+
+    const url = URL.createObjectURL(file);
+
+    preview.src = url;
+
+    preview.style.display = "block";
 
 });
